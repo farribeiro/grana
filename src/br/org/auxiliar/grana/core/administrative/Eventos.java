bde2573ef6d0ab39c329a8180173c0cf84f1b267
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2010-02-14 01:23:31 -0200

    Mover itens Eventos para core/Administrative

---
+++ src/br/org/auxiliar/grana/core/administrative/Eventos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.org.auxiliar.grana.core.administrative;
+
+import java.math.BigDecimal;
+
+/**
+ * 
+ * @author fabio
+ */
+public class Eventos {
+
+	private Evento evento;
+
+	public Eventos(String valor, String descricao, char tipoEvento) {
+		evento = new Evento(new BigDecimal(valor), descricao, tipoEvento);
+	}
+
+}
