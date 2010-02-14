fc1027b1ecf8e08b198b8eb10d84b61611ab3a2e
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2010-02-14 01:13:21 -0200

    Organize Imports

--- src/br/org/auxiliar/grana/core/financial/Eventos.java
+++ src/br/org/auxiliar/grana/core/financial/Eventos.java
@@ -7,16 +7,15 @@ package br.org.auxiliar.grana.core.financial;
 import java.math.BigDecimal;
 
 /**
- *
+ * 
  * @author fabio
  */
 public class Eventos {
 
-  private Evento evento;
+	private Evento evento;
 
-  public Eventos(String valor, String descricao, char tipoEvento) {
-    evento = new Evento(new BigDecimal(valor), descricao, tipoEvento);
-  }
-
+	public Eventos(String valor, String descricao, char tipoEvento) {
+		evento = new Evento(new BigDecimal(valor), descricao, tipoEvento);
+	}
 
 }
